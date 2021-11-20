7fdad06ad6a4c205a624018df6003309d9137771
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2021-11-20 13:27:27 +0200

    Commit  inicial do projecto ePatrimonio

---
+++ src/main/java/mz/gov/misau/EpatrimonioApplication.java
@@ -0,0 +1,13 @@
+package mz.gov.misau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpatrimonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpatrimonioApplication.class, args);
+	}
+
+}
